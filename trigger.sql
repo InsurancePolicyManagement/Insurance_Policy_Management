384eca6ba0e592d749f69a9e9a529a3daf08e6e8
Sakshi Nayak <90530768+Sakshi-Nayak@users.noreply.github.com>
2022-04-29 22:34:39 -0400

    Update trigger

--- trigger.sql
+++ trigger.sql
@@ -1,3 +1,26 @@
+CREATE OR REPLACE PROCEDURE remove_objects (
+    name_of_object VARCHAR2,
+    type_of_object VARCHAR2
+) IS
+    cnt NUMBER := 0;
+BEGIN
+    IF upper(type_of_object) = 'Trigger' THEN
+        SELECT
+            COUNT(*)
+        INTO cnt
+        FROM
+            user_tables
+        WHERE
+            upper(table_name) = upper(TRIM(name_of_object));
+
+        IF cnt > 0 THEN
+            EXECUTE IMMEDIATE 'drop Trigger ' || name_of_object || ' cascade constraints';
+        END IF;
+    END IF;
+
+END;
+/
+CALL remove_objects('update_customer_status', 'Trigger');
 set serveroutput on;
 CREATE OR REPLACE TRIGGER update_customer_status
 AFTER INSERT
@@ -13,6 +36,7 @@ END;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+CALL remove_objects('update_transaction_customer_status', 'Trigger');
 CREATE OR REPLACE TRIGGER update_transaction_customer_status
 AFTER INSERT
    ON TRANSACTION_ENTITY
@@ -28,6 +52,7 @@ END;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+CALL remove_objects('update_billing_customer_status', 'Trigger');
 CREATE OR REPLACE TRIGGER update_billing_customer_status
 AFTER INSERT
    ON ENTITY_BILLING
